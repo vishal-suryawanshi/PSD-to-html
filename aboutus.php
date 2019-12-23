c12f220825321c7bf86b940f779387e965d733f0
Internship <vishals2216@gmail.com>
2019-12-23 17:57:14 +0530

    about use php file

---
+++ aboutus.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>aboutus</title>
+</head>
+<body>
+	<?php 
+		include("header.php");
+	 ?>
+
+	 <div class="container" style="min-height: 430px;">
+	 	<div class="row">
+	 		<div class="col-md-8">
+	 			<div style="color:#804000; font-size: 20px;">Company Overview <hr style="margin-top: -2px"></div>
+	 			<p>Incepted in 1993, the M/S. Jaysing G. Hujare(Jadhav) today, is a strong, multifaceted, multi-disciplinary and vibrant organization, committed to delighting our customers through innovation. We strive to achieve the highest degree of technical excellence while maintaining the finest standards of business practices in all our endeavours. It is this trait that has always attracted the most talented people to our company.   While we seek to bring in newness and fresh energy into the organization, we’ve never under-valued our old relationships. All the stakeholders who have been with us through our journey and those who keep joining the family are a standing testimony of our beliefs.   Such strong teamwork, dedication and sheer hard work have ensured that we have won a major industry award every year since the day we started.   For us, this is just the beginning.</p>
+
+	 		</div>
+	 		<div class="col-md-4" style="border-left: 1px solid  #7f6942; position: relative; min-height: 450px;">
+	 			<div style="border-left: 1px;border-color: #7f9642; margin-left: 10px;margin-top: 25px;">
+	 				<div class="">
+	 					<div class="about-div">
+	 						<a href=""><h5>Company Overview</h5></a>
+	 					</div>
+	 					<div class="about-div">
+	 						<a href=""><h5>Director Message</h5></a>
+	 					</div>
+	 					<div class="about-div">
+	 						<a href=""><h5>Board</h5></a>
+	 					</div>
+	 					<div class="about-div">
+	 						<a href=""><h5>Quality Policy</h5></a>
+	 					</div>
+	 					<div class="about-div">
+	 						<a href=""><h5>Service We Provide</h5></a>
+	 					</div>
+	 					<!--<ul class="child_pages">								
+	 						<li class=""><a href="">Company Overview</a>
+							</li>
+							<li class=""><a href="" title="Director Message">Director Message</a>
+							</li>
+							<li class=""><a href="" title="Board">Board</a>
+							</li>
+							<li class=""><a href="" title="Quality Policy">Quality Policy</a>
+							</li>
+							<li class=""><a href="" title="Service We Provide">Service We Provide</a>
+							</li>
+						</ul>-->
+					</div>
+	 			</div>
+	 		</div>
+	 	</div>
+	 </div>
+
+	 <?php 
+		include("footer.php");
+	 ?>
+</body>
+</html>
