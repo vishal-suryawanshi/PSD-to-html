ac5d317b5ab5e06bf89c9c3f4cf56686aed72c44
Internship <vishals2216@gmail.com>
2019-12-24 17:51:25 +0530

    about us pahe

--- aboutus.php
+++ aboutus.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>aboutus</title>
+	<script src="one.js"></script>
 </head>
 <body>
 	<?php 
@@ -11,8 +12,8 @@
 	 <div class="container" style="min-height: 430px;">
 	 	<div class="row">
 	 		<div class="col-md-8">
-	 			<div style="color:#804000; font-size: 20px;">Company Overview <hr style="margin-top: -2px"></div>
-	 			<p>Incepted in 1993, the M/S. Jaysing G. Hujare(Jadhav) today, is a strong, multifaceted, multi-disciplinary and vibrant organization, committed to delighting our customers through innovation. We strive to achieve the highest degree of technical excellence while maintaining the finest standards of business practices in all our endeavours. It is this trait that has always attracted the most talented people to our company.   While we seek to bring in newness and fresh energy into the organization, we’ve never under-valued our old relationships. All the stakeholders who have been with us through our journey and those who keep joining the family are a standing testimony of our beliefs.   Such strong teamwork, dedication and sheer hard work have ensured that we have won a major industry award every year since the day we started.   For us, this is just the beginning.</p>
+	 			<div  font-size: 20px;">Company Overview <hr style="margin-top: -2px"></div>
+	 			<p >Incepted in 1993, the M/S. Jaysing G. Hujare(Jadhav) today, is a strong, multifaceted, multi-disciplinary and vibrant organization, committed to delighting our customers through innovation. We strive to achieve the highest degree of technical excellence while maintaining the finest standards of business practices in all our endeavours. It is this trait that has always attracted the most talented people to our company.   While we seek to bring in newness and fresh energy into the organization, we’ve never under-valued our old relationships. All the stakeholders who have been with us through our journey and those who keep joining the family are a standing testimony of our beliefs.   Such strong teamwork, dedication and sheer hard work have ensured that we have won a major industry award every year since the day we started.   For us, this is just the beginning.</p>
 
 	 		</div>
 	 		<div class="col-md-4" style="border-left: 1px solid  #7f6942; position: relative; min-height: 450px;">
