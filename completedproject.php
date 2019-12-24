640311a75902411b78d716240c98229dac4501a2
Internship <vishals2216@gmail.com>
2019-12-24 17:52:05 +0530

    completed project page

--- completedproject.php
+++ completedproject.php
@@ -2,13 +2,9 @@
 <html>
 <head>
 	<title>Completed project</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="one.js"></script>
-  <link rel="stylesheet" type="text/css" href="style.css">
-   <link rel="stylesheet" href="lightbox.min.css">
+	<link rel="stylesheet" type="text/css" href="lightbox.min.css">
+	<link rel="stylesheet" type="text/css" href="lightbox.css">
+	<script src="one.js"></script>
 </head>
 <script src="lightbox-plus-jquery.min.js"></script>
 <body style="background-color: #f9f4eb; margin-right: 0px;margin-bottom: 0px;">
@@ -21,7 +17,7 @@
 	<div class="container">
 		<div style="align-content: center;">
 				<div class="head-div">
-				<h3 class="head-p">Completed projects</h3><hr>
+				<h3 class="head-p" style="color: #80663a">Completed projects</h3><hr>
 				</div>
 				<div class="mapouter">
 					<div class="gmap_canvas">
@@ -32,21 +28,21 @@
 				<div class="col-md-4">
 					<figure>
 						<a href="images/1.jpg" data-lightbox="example-"><img class="img1 img-thumbnail" src="images/1.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px">Bhoi Samaj Nirman Sahakari Sanstha</figcaption>
+						<figcaption class="head-p" style="margin-left: 50px; color: #80663a">Bhoi Samaj Nirman Sahakari Sanstha</figcaption>
 						<hr>
 					</figure>
 				</div>
 				<div class="col-md-4">
 					<figure>
 						<a href="images/2.jpg" data-lightbox="example-"><img class="img-thumbnail img1" src="images/2.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px">Nikam Park</figcaption>
+						<figcaption class="head-p" style="margin-left: 50px;color: #80663a">Nikam Park</figcaption>
 						<hr>
 					</figure>
 				</div>
 				<div class="col-md-4">
 					<figure>
 						<a href="images/3.jpg" data-lightbox="example-"><img  class=" img1 img-thumbnail" src="images/2.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px">Shrikrishna Saraswati Bhaval</figcaption>
+						<figcaption class="head-p" style="margin-left: 50px;color: #80663a">Shrikrishna Saraswati Bhaval</figcaption>
 						<hr>
 					</figure>
 				</div>
@@ -56,14 +52,14 @@
 				<div class="col-md-4">
 					<figure>
 						<a href="images/4.jpg" data-lightbox="example-"><img class="img-thumbnail img1"  src="images/4.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px">Tara chembers</figcaption>
+						<figcaption class="head-p" style="margin-left: 50px;color: #80663a">Tara chembers</figcaption>
 						<hr>
 					</figure>
 				</div>
 				<div class="col-md-4">
 					<figure>
 						<a href="images/5.jpg" data-lightbox="example-"><img class="img-thumbnail img1"  src="images/5.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px">Tara Dham</figcaption>
+						<figcaption class="head-p" style="margin-left: 50px;color: #80663a">Tara Dham</figcaption>
 						<hr>
 					</figure>
 				</div>
@@ -74,7 +70,7 @@
 
 
 	<?php 
- 	include('header.php');
+ 	include('footer.php');
  ?>
 
 
