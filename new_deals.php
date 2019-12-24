91d427df2642149064baf187e72abc882e8d731e
Internship <vishals2216@gmail.com>
2019-12-24 17:54:26 +0530

    new deals page

--- new_deals.php
+++ new_deals.php
@@ -2,12 +2,7 @@
 <html>
 <head>
 	<title>new deals</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <script src="one.js"></script>
-  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body style="background-color: #f9f4eb; margin-right: 0px;margin-bottom: 0px;">
 <div>
@@ -18,39 +13,43 @@
  ?>
 
 	<div class="container">
+		<div class="container">
+		<div class="container">
 		<div style="align-content: center;">
 			<div class="head-div">
-				<h3 class="head-p">New Deals >> OM Nikam Estate</h3><hr>
+				<p class="head-p" style="font-size:20px ;color: #80663a;">New Deals >> OM Nikam Estate</p><hr style="margin-top: -2px;">
 			</div>
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-6 col-sm-12 col-xs-12">
 					<figure>
 						<img src="images/banner-small.jpg"><br><br>
 						<figcaption style="margin-left: 50px;"><a href="" class="textcol">A Project by Unity Promoters and Builders</a></figcaption>
 					</figure>
 				</div>
-				<div class="col-md-6">
-					<p style="font-size:12px"><strong> Company </strong>    :   <strong>Unity promoters and Builders pvt. ltd. </strong></p>
-					<p style="font-size:12px"> <strong>Address </strong>   :  1360 ‘B’ Ward, Vishnu Plaza,                    Khari corner,<br> New Mahadwar Road, Kolhapur – 416012</p>
-					<p style="font-size:12px"><strong>City</strong>    :    Kolhapur </p>
-					<p style="font-size:12px"><strong>For Booking</strong>   :  7350747200</p>
-					<p style="font-size:12px"><strong>Email</strong>   :   <a href="https://jaysinghujare.com/contact-us/" class="textcol">director@jaysinghujare.com </a></p>
+				<div class="col-md-6 col-sm-12 col-xs-12" style="margin-left: -30px">
+					<p style="font-size:12px; color: #80663a"><strong> Company </strong>    :   <strong>Unity promoters and Builders pvt. ltd. </strong></p>
+					<p style="font-size:12px; color: #80663a"> <strong>Address </strong>   :  1360 ‘B’ Ward, Vishnu Plaza,                    Khari corner,<br> New Mahadwar Road, Kolhapur – 416012</p>
+					<p style="font-size:12px; color: #80663a"><strong>City</strong>    :    Kolhapur </p>
+					<p style="font-size:12px ; color: #80663a"><strong>For Booking</strong>   :  7350747200</p>
+					<p style="font-size:12px; color: #80663a"><strong>Email</strong>   :   <a href="https://jaysinghujare.com/contact-us/" class="textcol">director@jaysinghujare.com </a></p>
 				</div>
 			</div><br>
-			<p style="font-size: 16px"><a class="textcol" style="font-size: 16px;" href="#">OM Nikam Estate</a> Is located in the heart of kolhapur city, 1.5 km from Mahalaxmi temple, near about Rankala Talav in Devakar panad.</p><br>
+			<p style="font-size: 16px;color: #80663a"><a class="textcol" style="font-size: 16px;" href="#">OM Nikam Estate</a> Is located in the heart of kolhapur city, 1.5 km from Mahalaxmi temple, near about Rankala Talav in Devakar panad.</p><br>
 
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-6 col-sm-12 col-xs-12">
 					<figure>
 						<img src="images/om.jpg"><br><br>
 					</figure>
 				</div>
-				<div class="col-md-6">
-					<p style="font-size: 20px">2BHK Delux Flats</p>
-					<p style="font-size:12px"><strong>Site address</strong>   :   : Om Nikam Esate, 930/3, Devakar Panad, near Nikam Park, Kolhapur.
+				<div class="col-md-6 col-sm-12 col-xs-12" style="margin-left: -30px">
+					<p style="font-size: 20px; color: #80663a">2BHK Delux Flats</p>
+					<p style="font-size:12px; color: #80663a"><strong>Site address</strong>   :   : Om Nikam Esate, 930/3, Devakar Panad, near Nikam Park, Kolhapur.
 
 </p>
 				</div>
+			</div>
+			</div>
 			</div>
 		</div>
 	</div>
