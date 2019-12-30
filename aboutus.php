5dab8ccac64f929a606393e47b45719f6fb004be
unknown <vishals2216@gmail.com>
2019-12-30 17:38:06 +0530

    about us php page

--- aboutus.php
+++ aboutus.php
@@ -6,33 +6,34 @@
 </head>
 <body>
 	<?php 
-		include("header.php");
+		include("header1.php");
 	 ?>
 
-	 <div class="container" style="min-height: 430px;">
+	 <div class="container">
+	 	<div class="cont2">
 	 	<div class="row">
-	 		<div class="col-md-8">
-	 			<div  font-size: 20px;">Company Overview <hr style="margin-top: -2px"></div>
-	 			<p >Incepted in 1993, the M/S. Jaysing G. Hujare(Jadhav) today, is a strong, multifaceted, multi-disciplinary and vibrant organization, committed to delighting our customers through innovation. We strive to achieve the highest degree of technical excellence while maintaining the finest standards of business practices in all our endeavours. It is this trait that has always attracted the most talented people to our company.   While we seek to bring in newness and fresh energy into the organization, we’ve never under-valued our old relationships. All the stakeholders who have been with us through our journey and those who keep joining the family are a standing testimony of our beliefs.   Such strong teamwork, dedication and sheer hard work have ensured that we have won a major industry award every year since the day we started.   For us, this is just the beginning.</p>
+	 		<div class="col-md-8 about2-div" style="border-right: thin solid #DFD9CC; height: auto; position: relative; min-height: 520px;">
+	 			<h3 style="width: 671px;">Company Overview </h3>
+	 			<p style="margin-top: 15px; margin-left: 13px;">Incepted in 1993, the M/S. Jaysing G. Hujare(Jadhav) today, is a strong, multifaceted, multi-disciplinary and vibrant organization, committed to delighting our customers through innovation. We strive to achieve the highest degree of technical excellence while maintaining the finest standards of business practices in all our endeavours. It is this trait that has always attracted the most talented people to our company.   While we seek to bring in newness and fresh energy into the organization, we’ve never under-valued our old relationships. All the stakeholders who have been with us through our journey and those who keep joining the family are a standing testimony of our beliefs.   Such strong teamwork, dedication and sheer hard work have ensured that we have won a major industry award every year since the day we started.   For us, this is just the beginning.</p>
 
 	 		</div>
-	 		<div class="col-md-4" style="border-left: 1px solid  #7f6942; position: relative; min-height: 450px;">
-	 			<div style="border-left: 1px;border-color: #7f9642; margin-left: 10px;margin-top: 25px;">
-	 				<div class="">
+	 		<div class="col-md-4 about2-div" >
+	 			<div>
+	 				<div class="about3-div">
 	 					<div class="about-div">
-	 						<a href=""><h5>Company Overview</h5></a>
+	 						<a href="#">Company Overview</a>
 	 					</div>
 	 					<div class="about-div">
-	 						<a href=""><h5>Director Message</h5></a>
+	 						<a href="#">Director Message</a>
 	 					</div>
 	 					<div class="about-div">
-	 						<a href=""><h5>Board</h5></a>
+	 						<a href="#">Board</a>
 	 					</div>
 	 					<div class="about-div">
-	 						<a href=""><h5>Quality Policy</h5></a>
+	 						<a href="#">Quality Policy</a>
 	 					</div>
 	 					<div class="about-div">
-	 						<a href=""><h5>Service We Provide</h5></a>
+	 						<a href="#">Service We Provide</a>
 	 					</div>
 	 					<!--<ul class="child_pages">								
 	 						<li class=""><a href="">Company Overview</a>
@@ -51,9 +52,9 @@
 	 		</div>
 	 	</div>
 	 </div>
-
+</div>
 	 <?php 
-		include("footer.php");
+		include("footer1.php");
 	 ?>
 </body>
 </html>
