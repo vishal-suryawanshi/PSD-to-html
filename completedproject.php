6957db1e92e92c6ed5d8cb9befd738f1f1a2bdce
unknown <vishals2216@gmail.com>
2019-12-30 17:38:43 +0530

    completed project php page

--- completedproject.php
+++ completedproject.php
@@ -7,60 +7,60 @@
 	<script src="one.js"></script>
 </head>
 <script src="lightbox-plus-jquery.min.js"></script>
-<body style="background-color: #f9f4eb; margin-right: 0px;margin-bottom: 0px;">
+<body>
 <div>
 	
 	<?php 
- 	include('header.php');
+ 	include('header1.php');
  ?>
 
 	<div class="container">
-		<div style="align-content: center;">
+		<div class="cont2">
 				<div class="head-div">
-				<h3 class="head-p" style="color: #80663a">Completed projects</h3><hr>
+				<h3 style="font-size: 17px">Completed projects</h3>
 				</div>
 				<div class="mapouter">
-					<div class="gmap_canvas">
+					<div class="gmap_canvas map">
 						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d122283.79110568027!2d74.16882740658711!3d16.708456761663925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1000cdec07a29%3A0xece8ea642952e42f!2sKolhapur%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1576839469641!5m2!1sen!2sin" width="100%" height="500" frameborder="0" style="border:0;" allowfullscreen=""></iframe>" 
 					</div>
-				</div><br><br>
-			<div class="row">
+				</div>
+			<div class="row"s>
 				<div class="col-md-4">
 					<figure>
 						<a href="images/1.jpg" data-lightbox="example-"><img class="img1 img-thumbnail" src="images/1.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px; color: #80663a">Bhoi Samaj Nirman Sahakari Sanstha</figcaption>
-						<hr>
+						<figcaption class="completed-figcap">Bhoi Samaj Nirman Sahakari Sanstha</figcaption>
+						<h3></h3>
 					</figure>
 				</div>
 				<div class="col-md-4">
 					<figure>
 						<a href="images/2.jpg" data-lightbox="example-"><img class="img-thumbnail img1" src="images/2.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px;color: #80663a">Nikam Park</figcaption>
-						<hr>
+						<figcaption class="completed-figcap">Nikam Park</figcaption>
+						<h3></h3>
 					</figure>
 				</div>
 				<div class="col-md-4">
 					<figure>
 						<a href="images/3.jpg" data-lightbox="example-"><img  class=" img1 img-thumbnail" src="images/2.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px;color: #80663a">Shrikrishna Saraswati Bhaval</figcaption>
-						<hr>
+						<figcaption class="completed-figcap">Shrikrishna Saraswati Bhaval</figcaption>
+						<h3></h3>
 					</figure>
 				</div>
 			</div>
-
+				<br>
 			<div class="row">
 				<div class="col-md-4">
 					<figure>
 						<a href="images/4.jpg" data-lightbox="example-"><img class="img-thumbnail img1"  src="images/4.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px;color: #80663a">Tara chembers</figcaption>
-						<hr>
+						<figcaption class="completed-figcap">Tara chembers</figcaption>
+						<h3></h3>
 					</figure>
 				</div>
 				<div class="col-md-4">
 					<figure>
 						<a href="images/5.jpg" data-lightbox="example-"><img class="img-thumbnail img1"  src="images/5.jpg"><br><br></a>
-						<figcaption class="head-p" style="margin-left: 50px;color: #80663a">Tara Dham</figcaption>
-						<hr>
+						<figcaption class="completed-figcap">Tara Dham</figcaption>
+						<h3></h3>
 					</figure>
 				</div>
 			</div>
@@ -70,7 +70,7 @@
 
 
 	<?php 
- 	include('footer.php');
+ 	include('footer1.php');
  ?>
 
 
