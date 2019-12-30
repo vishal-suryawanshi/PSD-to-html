fb425ea8fb44f205da00a171fe85e94b372c4867
unknown <vishals2216@gmail.com>
2019-12-30 17:45:31 +0530

    home page

--- index.php
+++ index.php
@@ -5,71 +5,64 @@
   <title>home</title>
   <script src="one.js"></script> 
 </head>
-<body style="background-color: #f9f4eb; margin-right: 0px;margin-bottom: 0px;">
+<body>
   <?php 
-  include('header.php');
+  include('header1.php');
  ?>
   <div >
 <div class="row">
      <div class="col-sm-12 col-xs-12 col-md-2">
        
      </div>
-     <div class="col-md-8 col-sm-12 col-xs-12">
-                      <div  id="myCarousel" class="mm carousel slide" data-ride="carousel">
-                  <!-- Indicators -->
+     <div class="col-md-8 col-sm-12 col-xs-12" style="margin-top: 30px;">
+                  <div  id="myCarousel" class=" carousel slide" data-ride="carousel" style="overflow: hidden;width: 100%">
+                
                     <ol class="carousel-indicators">
                       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                       <li data-target="#myCarousel" data-slide-to="1"></li>
                       <li data-target="#myCarousel" data-slide-to="2"></li>
                     </ol>
 
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
+                    
+                    <div class="carousel-inner border1" style="border: 11px solid #fff">
                       <div class="item active">
-                        <img src="images/banner6.jpg" class="img-thumbnail" alt="" style="width:100%;">
+                        <img src="images/banner6.jpg"  alt="" style="width:100%; border-width:4px solid black">
                       </div>
 
                       <div class="item">
-                        <img src="images/slider_img5.gif" class="img-thumbnail" alt="" style="width:100%;">
+                        <img src="images/slider_img5.gif" alt="" style="width:100%;border-width:4px solid black">
                       </div>
                     
                       <div class="item">
-                        <img src="images/slider_img6.gif" class="img-thumbnail" alt="" style="width:100%;">
+                        <img src="images/slider_img6.gif" alt="" style="width:100%;border-width: 4px solid black">
                       </div>
                     </div>
-
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                      <span class="glyphicon glyphicon-chevron-left"></span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                      <span class="glyphicon glyphicon-chevron-right"></span>
-                      <span class="sr-only">Next</span>
-                    </a>
                   </div>    
 
-                  <div class="row" style="margin-top: 20px;">
+                  <div class="row" style="margin-top: 30px;">
 
                   <div class="col-md-4 col-sm-12 col-xs-12">
                     <div class="div5 index1" >
-                      <h3 class="index1" style="font-size: 20px">Director Message</h3>
-                      <hr>
-                      <p class="index1">Its’ heartening to see some signal of global economic revival. Though too early to say so, but I am optimistic about the revival of economy earlier than estimated. With our persistent efforts I am happy to see the</p>
+                      <h3 class="index1">Director Message</h3>
+                      <p class="index1 why-us">Its’ heartening to see some signal of global economic revival. Though too early to say so, but I am optimistic about the revival of economy earlier than estimated. With our persistent efforts I am happy to see the</p>
                     </div>
                   </div> 
                   <div class="col-md-4 col-sm-12 col-xs-12 l">
                     <div class="div5 index1">
-                     <h3 style="font-size: 20px">Why us..?</h3>
-                     <hr>                      >>New Deal<br>>>Service<br>>>Quqality<br>>>Want Help
+                     <h3 >Why us..?</h3>
+                     <ul class="why-us">
+                         <li>New Deal</li>
+                         <li>Service</li>
+                         <li>Quality</li>
+                         <li>Want Help</li>
+                     </ul>
                   </div>
                   </div> 
                   <div class="col-md-4 col-sm-12 col-xs-12 ">
                     <div class="div5 index1">
-                     <h3 class="index1" style="font-size: 20px">Testimonial</h3>
-                      <hr>
-                     <!-- <p class="index1"><q>Human beings, by changing the inner attitudes of their minds, can change the outer aspects of their lives… .</q><br><p style="float: right; color: #80663a;">– William James</p></p>-->
-                     <blockquote>Human beings, by changing the inner attitudes of their minds, can change the outer aspects of their lives… .</blockquote><br><p style="float: right; color: #80663a;">– William James</p>
+                     <h3 class="index1" >Testimonial</h3>
+                     
+                     <blockquote class="why-us">Human beings, by changing the inner attitudes of their minds, can change the outer aspects of their lives… .</blockquote><br><p style="float: right;font-size: 13px;">– William James</p>
                     </div>
                   </div>                    
                   </div>  
@@ -83,5 +76,5 @@
 </div>
 </body>
 <?php 
- 	include('footer.php');
+ 	include('footer1.php');
  ?>
