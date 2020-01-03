39b156b3d1201b670c4339a42e6c0116b00fff9e
unknown <vishals2216@gmail.com>
2020-01-03 09:12:16 +0530

    home page php page

--- index.php
+++ index.php
@@ -2,29 +2,25 @@
  <!DOCTYPE html>
 <html>
 <head>
-  <title>home</title>
-  <script src="one.js"></script> 
+  <title>home</title> 
 </head>
 <body>
   <?php 
   include('header1.php');
  ?>
-  <div >
+  <div class="container">
 <div class="row">
-     <div class="col-sm-12 col-xs-12 col-md-2">
-       
-     </div>
-     <div class="col-md-8 col-sm-12 col-xs-12" style="margin-top: 30px;">
+     <div class="col-md-12 col-sm-12 col-xs-12" style="margin-top: 30px;">
                   <div  id="myCarousel" class=" carousel slide" data-ride="carousel" style="overflow: hidden;width: 100%">
                 
                     <ol class="carousel-indicators">
-                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="0" ></li>
                       <li data-target="#myCarousel" data-slide-to="1"></li>
                       <li data-target="#myCarousel" data-slide-to="2"></li>
                     </ol>
 
                     
-                    <div class="carousel-inner border1" style="border: 11px solid #fff">
+                    <div class="carousel-inner border1" >
                       <div class="item active">
                         <img src="images/banner6.jpg"  alt="" style="width:100%; border-width:4px solid black">
                       </div>
@@ -42,13 +38,13 @@
                   <div class="row" style="margin-top: 30px;">
 
                   <div class="col-md-4 col-sm-12 col-xs-12">
-                    <div class="div5 index1" >
+                    <div class="index-col index1" >
                       <h3 class="index1">Director Message</h3>
                       <p class="index1 why-us">Its’ heartening to see some signal of global economic revival. Though too early to say so, but I am optimistic about the revival of economy earlier than estimated. With our persistent efforts I am happy to see the</p>
                     </div>
                   </div> 
                   <div class="col-md-4 col-sm-12 col-xs-12 l">
-                    <div class="div5 index1">
+                    <div class="index-col index1">
                      <h3 >Why us..?</h3>
                      <ul class="why-us">
                          <li>New Deal</li>
@@ -59,19 +55,14 @@
                   </div>
                   </div> 
                   <div class="col-md-4 col-sm-12 col-xs-12 ">
-                    <div class="div5 index1">
+                    <div class="index-col index1">
                      <h3 class="index1" >Testimonial</h3>
                      
-                     <blockquote class="why-us">Human beings, by changing the inner attitudes of their minds, can change the outer aspects of their lives… .</blockquote><br><p style="float: right;font-size: 13px;">– William James</p>
+                     <blockquote class="why-us">Human beings, by changing the inner attitudes of their minds, can change the outer aspects of their lives… .</blockquote><br><p style="float: right;font-size: 13px; color: #7f6942;">– William James</p>
                     </div>
                   </div>                    
-                  </div>  
-       
+                  </div>     
      </div>
-     <div class="col-md-2 col-sm-12 col-xs-12">
-       
-     </div>
-
 </div>
 </div>
 </body>
