467a322ebbadcc2d89ebe548d063135713c74da6
unknown <vishals2216@gmail.com>
2020-01-03 09:11:24 +0530

    final footer php page

--- footer1.php
+++ footer1.php
@@ -7,25 +7,24 @@
 <div class="footer_up"></div>
 
 <footer class="site-foot">
-  <div class="container">
-    <div class="cont">
+    <div class="container">
         <div class="row">
             <div class="col-md-7 col-sm-12 col-xs-12">
                 <nav class="main-header">
                   <div class="footer-menu">
-                      <ul class="nav navbar-nav foot">
+                      <ul class="nav navbar-nav foot-link">
                         <li><a  href="index.php">Home</a></li>
-                        <li><a  href="currProj.php">Current Projects</a></li>
-                        <li><a  href="CompletedProj.php">Completed Projects</a></li>
-                        <li><a  href="contact.php">Contact Us</a></li>
-                        <li><a  href="about.php">About Us</a></li>
+                        <li><a  href="currentproject.php">Current Projects</a></li>
+                        <li><a  href="completedproject.php">Completed Projects</a></li>
+                        <li><a  href="contactus.php">Contact Us</a></li>
+                        <li><a  href="aboutus.php">About Us</a></li>
                       </ul>
                   </div>
                 </nav>
             </div>
             <div class="col-md-5 col-sm-12 col-xs-12">
               <div class="foot2"><p class="copyrights">Copyright @ 2011- 2019 .
-              <strong> M/S.Jaysing G. Hujare (Jadhav)</strong>, Kolhapur.
+               <span> M/S.Jaysing G. Hujare (Jadhav),</span> Kolhapur.
               </p></div>
             </div>
         </div>
@@ -33,13 +32,12 @@
             <div class="foot3 col-md-12 col-sm-12 col-xs-12 ">
               <p class="foot-end">
                 Website designed by
-                <a href="https://www.makarandmane.com" target="_blank"><stromg>Makarand Mane</stromg></a>
+                <a href="https://www.makarandmane.com" target="_blank" style="font-family: sans-serif;"><stromg>Makarand Mane</stromg></a>
                 Powered by
-                <a href="https://www.tycheventures.com" target="_blank"><stromg>Tyche Ventures</stromg></a>
+                <a href="https://www.tycheventures.com" target="_blank" style="font-family: sans-serif;"><stromg>Tyche Ventures</stromg></a>
               </p>
             </div>
         </div>
-      </div>
       </div>
   </footer>
 </body>
