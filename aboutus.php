7e635c8e603519e1bee1b27edc24a321d216fe1e
unknown <vishals2216@gmail.com>
2020-01-03 09:09:16 +0530

    about us php page

--- aboutus.php
+++ aboutus.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 	<title>aboutus</title>
-	<script src="one.js"></script>
 </head>
 <body>
 	<?php 
@@ -10,10 +9,10 @@
 	 ?>
 
 	 <div class="container">
-	 	<div class="cont2">
+	 	
 	 	<div class="row">
 	 		<div class="col-md-8 about2-div" style="border-right: thin solid #DFD9CC; height: auto; position: relative; min-height: 520px;">
-	 			<h3 style="width: 671px;">Company Overview </h3>
+	 			<h3 style="width: 650px;">Company Overview </h3>
 	 			<p style="margin-top: 15px; margin-left: 13px;">Incepted in 1993, the M/S. Jaysing G. Hujare(Jadhav) today, is a strong, multifaceted, multi-disciplinary and vibrant organization, committed to delighting our customers through innovation. We strive to achieve the highest degree of technical excellence while maintaining the finest standards of business practices in all our endeavours. It is this trait that has always attracted the most talented people to our company.   While we seek to bring in newness and fresh energy into the organization, we’ve never under-valued our old relationships. All the stakeholders who have been with us through our journey and those who keep joining the family are a standing testimony of our beliefs.   Such strong teamwork, dedication and sheer hard work have ensured that we have won a major industry award every year since the day we started.   For us, this is just the beginning.</p>
 
 	 		</div>
@@ -35,23 +34,11 @@
 	 					<div class="about-div">
 	 						<a href="#">Service We Provide</a>
 	 					</div>
-	 					<!--<ul class="child_pages">								
-	 						<li class=""><a href="">Company Overview</a>
-							</li>
-							<li class=""><a href="" title="Director Message">Director Message</a>
-							</li>
-							<li class=""><a href="" title="Board">Board</a>
-							</li>
-							<li class=""><a href="" title="Quality Policy">Quality Policy</a>
-							</li>
-							<li class=""><a href="" title="Service We Provide">Service We Provide</a>
-							</li>
-						</ul>-->
-					</div>
+	 				</div>
 	 			</div>
 	 		</div>
 	 	</div>
-	 </div>
+	 
 </div>
 	 <?php 
 		include("footer1.php");
