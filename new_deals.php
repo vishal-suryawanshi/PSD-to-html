0c808129dd4042ca8d43d2529a74b193f5f48de1
unknown <vishals2216@gmail.com>
2020-01-03 09:12:56 +0530

    new deals php page

--- new_deals.php
+++ new_deals.php
@@ -2,39 +2,34 @@
 <html>
 <head>
 	<title>new deals</title>
-  <script src="one.js"></script>
 </head>
 <body>
-<div>
-	
 	<?php 
-	
  	include('header1.php');
- ?>
+ 	?>
 
 	<div class="container">
-		<div class="cont2">
-		<div>
-		<div style="align-content: center;">
-			<div class="head-div">
+		
+		<div style="align-content: center;" class="">
+			<div class="head-div-title">
 				<h3>New Deals >> OM Nikam Estate</h3>
 			</div>
-			<div class="row" >
-				<div class="col-md-6 col-sm-12 col-xs-12">
+			<div class="row" style="margin-top: 30px;">
+				<div class="col-md-6 col-sm-12 col-xs-12 new-deals" >
 					<figure>
 						<img src="images/banner-small.jpg"><br><br>
 						<figcaption style="margin-left: 50px;"><a href="" class="textcol">A Project by Unity Promoters and Builders</a></figcaption>
 					</figure>
 				</div>
-				<div class="col-md-6 col-sm-12 col-xs-12" style="margin-left: -30px">
+				<div class="col-md-6 col-sm-12 col-xs-12 new-deals" style="margin-left: -30px">
 					<p ><strong> Company </strong>    :   <strong>Unity promoters and Builders pvt. ltd. </strong></p>
 					<p > <strong>Address </strong>   :  1360 ‘B’ Ward, Vishnu Plaza,                    Khari corner,<br> New Mahadwar Road, Kolhapur – 416012</p>
 					<p><strong>City</strong>    :    Kolhapur </p>
 					<p ><strong>For Booking</strong>   :  7350747200</p>
-					<p ><strong>Email</strong>   :   <a href="https://jaysinghujare.com/contact-us/" class="textcol">director@jaysinghujare.com </a></p>
+					<p ><strong>Email</strong>   :   <a href="https://jaysinghujare.com/contact-us/" class="textcol text-decoration">director@jaysinghujare.com </a></p>
 				</div>
 			</div><br>
-			<p class="pp"><a class="textcol" href="#">OM Nikam Estate</a> Is located in the heart of kolhapur city, 1.5 km from Mahalaxmi temple, near about Rankala Talav in Devakar panad.</p><br>
+			<p class="pp-newdeals newdeals-p"><a class="textcol text-decoration" href="#">OM Nikam Estate</a> Is located in the heart of kolhapur city, 1.5 km from Mahalaxmi temple, near about Rankala Talav in Devakar panad.</p><br>
 
 			<div class="row">
 				<div class="col-md-6 col-sm-12 col-xs-12">
@@ -42,27 +37,15 @@
 						<img src="images/om.jpg"><br><br>
 					</figure>
 				</div>
-				<div class="col-md-6 col-sm-12 col-xs-12" style="margin-left: -30px">
-					<p class="newdeals-p">2BHK Delux Flats</p>
-					<p style="margin-top: 15px;"><strong>Site address</strong>   :   : Om Nikam Esate, 930/3, Devakar Panad, near Nikam Park, Kolhapur.
-
-</p>
+				<div class="col-md-6 col-sm-12 col-xs-12 new-deals" style="margin-left: -30px">
+					<p style="font-size: 15px;">2BHK Delux Flats</p>
+					<p style="margin-top: 15px;"><strong>Site address</strong>   :    Om Nikam Esate, 930/3, Devakar Panad, near Nikam Park, Kolhapur.</p>
 				</div>
-			</div>
-			</div>
 			</div>
 		</div>
 	</div>
-
-
-
 	<?php 
  	include('footer1.php');
  ?>
-
-
-</div>
-
-
 </body>
 </html>
